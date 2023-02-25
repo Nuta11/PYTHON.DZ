d08d1e74248edfdce72c684d2164252a76dedeac
Anna N <nearonskaya@yandex.ru>
2023-02-25 20:55:31 +0300

    first commit

---
+++ DZ1/zadacha1.py
@@ -0,0 +1,13 @@
+
+number = input('Введите любое трёхзначное число: ')
+if len(number) == 4 and number[0] == '-':
+        print('Сумма цифр данного числа равна ', int(number[1]) + int(number[2]) + int(number[3])) # Минус не считаем, т. к. математически минус - не цифра
+elif len(number) != 3 or (len(number) == 3 and number[0] == '0'):
+    print('Читайте внимательнее условие! Число должно быть трёхзначным!')
+else:
+    for i in range(len(number)):
+        if number[i] in 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZабвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ!"№;:*()_-=+/<>[]{&^$}#@~':
+            print('Вы ввели не число!')
+            break
+    else:
+        print('Сумма цифр данного числа равна ', int(number[0]) + int(number[1]) + int(number[2]))
